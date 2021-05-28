849aa26863b53cce3d63a8ad1e89ada9ca07a612
OmidTarkhaneh <otarkhaneh@mun.ca>
2021-05-28 11:13:07 -0230

    report file added

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,27 @@
 # report.py
 #
 # Exercise 2.4
+
+# pcost.py
+#
+# Exercise 1.30
+
+
+def read_portfolio(filename):
+    """ This function return total cost of purchasing price of shares
+    """
+    portfolio=[]
+
+    f=open(filename,'rt')
+    headers=next(f)
+
+    for line in f:
+        row=line.split(',')
+        holding=(row[0],int(row[1]),float(row[2]))
+        portfolio.append(holding)
+
+    return portfolio    
+
+
+portfolio = read_portfolio('Data/portfolio.csv')
+print('portfolio:', portfolio)
