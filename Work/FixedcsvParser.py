464089cccedd05434ad6b992385d2590dfb9172d
OmidTarkhaneh <otarkhaneh@mun.ca>
2021-05-29 22:55:00 -0230

    FixedcsvParser file added

---
+++ Work/FixedcsvParser.py
@@ -0,0 +1,55 @@
+
+import csv
+
+def parse_csv(lines,select=None,types=None, is_header=False , silence_errors=True):
+    '''
+    This function parse a csv file into a list of records and filter it based on select and type options
+    '''
+
+
+    rows=csv.reader(lines)
+
+    # Read the file header
+    headers=next(rows) if is_header else []
+
+    records=[]
+    try:
+        if select:
+            idx=[headers.index(cols) for cols in select]
+            headers=select
+            
+    except:
+        if not silence_errors:
+            print("select argument requires column headers")        
+  
+    try:
+        for row in rows:
+            if not row:
+                continue  # skip rows with no data
+            # indices = [ headers.index(colname) for colname in select ]
+            
+                # records.append(record)
+            if select:
+                row=[row[idxs] for idxs in idx]
+                
+            if types:
+                row = [func(val) for func, val in zip(types, row) ]      
+            
+            if is_header:
+                record = dict(zip(headers, row))
+            
+            else:
+                record=tuple(row)
+
+            records.append(record)    
+    except Exception as e:
+            if not silence_errors:
+                print('Failed : Reason--->', e)
+                
+            
+    return records
+
+
+
+
+       
