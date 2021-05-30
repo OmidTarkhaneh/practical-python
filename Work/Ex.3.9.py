16a9cd00463d4ce25872c37f1cf0e469cf3bfeba
OmidTarkhaneh <otarkhaneh@mun.ca>
2021-05-29 22:52:15 -0230

    Exercise 3.9 added

---
+++ Work/Ex.3.9.py
@@ -0,0 +1,54 @@
+# Exercise 3.9
+
+import csv
+
+def parse_csv(filename,select=None,types=None, is_header=False):
+    '''
+    This function parse a csv file into a list of records and filter it based on select and type options
+    '''
+
+    with open(filename) as f:
+        rows=csv.reader(f)
+
+        # Read the file header
+        headers=next(rows) if is_header else []
+        # if not is_header:
+        #     headers=[]
+
+        records=[]
+        try:
+            if select:
+                idx=[headers.index(cols) for cols in select]
+                headers=select
+        except:
+            raise RuntimeError("select argument requires column headers")        
+  
+        try:
+            for row in rows:
+                if not row:
+                    continue  # skip rows with no data
+                # indices = [ headers.index(colname) for colname in select ]
+                
+                    # records.append(record)
+                if select:
+                    row=[row[idxs] for idxs in idx]
+                    
+                if types:
+                    row = [func(val) for func, val in zip(types, row)]     
+                
+                if is_header:
+                    record = dict(zip(headers, row))
+                
+                else:
+                    record=tuple(row)
+
+                records.append(record)    
+        except ValueError as e:
+                 print('Failed : Reason--->', e)
+                  
+        
+    
+    return records
+
+records=parse_csv('Data/missing.csv',types=[str,int],is_header=False)           
+print(records)
